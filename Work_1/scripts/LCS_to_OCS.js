b60dd5a9943209328f8115045362cb936ae2edfa
12401453 <12401453@protonmail.com>
2024-10-19 21:48:08 +0100

    ..

--- Work_1/scripts/LCS_to_OCS.js
+++ Work_1/scripts/LCS_to_OCS.js
@@ -8,6 +8,11 @@ const input_file = readline.createInterface({input: read_stream});
 const front_vowels = ['i', 'e', 'ę', 'ь', 'ŕ̥', 'ĺ̥'];
 
 const ORT_regex = /[eo][rl]([tŕrpsšdfgћђklĺzžxčvbnńmǯ]|$)/
+const PV2_regex = /[kgx](?:[ěęeiь]|ŕ̥|ĺ̥)/;
+const PV2_map = new Map();
+PV2_map.set('k', 'c');
+PV2_map.set('g', 'ʒ');
+PV2_map.set('x', 'ś');
 
 const mappings = {
   'omъjimъ' : 'ъіимъ',
@@ -29,15 +34,18 @@ const mappings = {
   'ŕǞ' : 'рꙗ',
   'ńǞ' : 'нꙗ',
   'ĺǞ' : 'лꙗ',
+  'śa' : 'сꙗ',
   'jǞ' : 'ꙗ',
   'ŕu' : 'рю',
   'ńu' : 'ню',
   'ĺu' : 'лю',
+  'śu' : 'сю',
   'ju' : 'ю',
   'ŕǫ' : 'рѭ',
   'ńǫ' : 'нѭ',
   'ĺǫ' : 'лѭ',
   'jǫ' : 'ѭ',
+  'śǫ' : 'сѭ',
   'ŕe' : 'р҄е',
   'ŕi' : 'р҄и',
   'ŕь' : 'р҄ь', 
@@ -54,8 +62,8 @@ const mappings = {
   'ŕ̥' : 'рь',
   'r̥' : 'ръ',
   'ĺ̥' : 'ль',
-  'l̥' : 'лъ'
-  /*'' : '',
+  'l̥' : 'лъ',
+  '' : '',
   '' : '',
   '' : '',
   '' : '',
@@ -85,19 +93,24 @@ const mappings = {
   '' : '', 
   '' : '', 
   '' : '', 
-  '' : '' */
+  '' : '' 
 }
 
 const convertToOCS = (lcs_word) => {
   let ORT_pos =  lcs_word.search(ORT_regex);
+  let PV2_pos = lcs_word.search(PV2_regex);
   while(ORT_pos != -1) {
     const ort_vowel = lcs_word.at(ORT_pos);
     const ort_liquid = lcs_word.at(ORT_pos + 1);
     const lengthened_vowel = ort_vowel == 'e' ? 'ě' : 'a';
-
     lcs_word = lcs_word.slice(0, ORT_pos) + ort_liquid + lengthened_vowel + lcs_word.slice(ORT_pos + 2);
     ORT_pos = lcs_word.search(ORT_regex);
-
+  }
+  while(PV2_pos != -1) {
+    const PV2_vowel = lcs_word.at(PV2_pos + 1);
+    const PV2_cons = lcs_word.at(PV2_pos);
+    lcs_word = lcs_word.slice(0, PV2_pos) + PV2_map.get(PV2_cons) + lcs_word.slice(PV2_pos + 1);
+    PV2_pos = lcs_word.search(PV2_regex);
   }
   
   for(const key in mappings) {
@@ -108,7 +121,8 @@ const convertToOCS = (lcs_word) => {
 
 // input_file.on('line', line => fs.appendFileSync("copy.csv", line+'\n'));
 input_file.on('line', line => {
-  if(line.search(ORT_regex) != -1) line += "|TORT-group";
+  if(line.search(ORT_regex) != -1) line += "|TORT";
+  if(line.search(PV2_regex) != -1) line += "|PV2";
   console.log(convertToOCS(line));
 });
 
