6ca0f028712b462874e52c251f0a0ff9f0eee2e2
12401453 <12401453@protonmail.com>
2024-10-20 01:36:17 +0100

    ..

--- Work_1/scripts/LCS_to_OCS.js
+++ Work_1/scripts/LCS_to_OCS.js
@@ -9,10 +9,24 @@ const front_vowels = ['i', 'e', 'ę', 'ь', 'ŕ̥', 'ĺ̥'];
 
 const ORT_regex = /[eo][rl]([tŕrpsšdfgћђklĺzžxčvbnńmǯ]|$)/
 const PV2_regex = /[kgx](?:[ěęeiь]|ŕ̥|ĺ̥)/;
+const PV3_regex = /[ьi][kgx][auǫ]/;
+
 const PV2_map = new Map();
 PV2_map.set('k', 'c');
 PV2_map.set('g', 'ʒ');
 PV2_map.set('x', 'ś');
+
+const applyPV3 = (lcs_form) => {
+  let PV3_pos = lcs_form.search(PV3_regex);
+  while(PV3_pos != -1) {
+    const velar = lcs_form.at(PV3_pos + 1);
+    lcs_form = lcs_form.slice(0, PV3_pos + 1) + PV2_map.get(velar) + lcs_form.slice(PV3_pos + 2);
+    PV3_pos = lcs_form.search(PV3_regex);
+  }
+
+  return lcs_form;
+
+};
 
 const mappings = {
   'omъjimъ' : 'ъіимъ',
@@ -59,41 +73,91 @@ const mappings = {
   'ĺь' : 'л҄ь',
   'ĺę' : 'лѩ',
   'ję' : 'ѩ',
+  'je' : 'ѥ',
+  'ji' : 'и',
+  'jь' : 'и',
+  'ьj' : 'и',
+  'jo' : 'о',
+  'jě' : 'ѣ',
+  '' : '',
   'ŕ̥' : 'рь',
   'r̥' : 'ръ',
   'ĺ̥' : 'ль',
   'l̥' : 'лъ',
-  '' : '',
-  '' : '',
-  '' : '',
-  '' : '',
-  '' : '',
-  '' : '',
-  '' : '',
-  '' : '',
-  '' : '',
-  '' : '',
-  '' : '',
   'šč' : 'щ',
   'šћ' : 'щ',
   'žǯ' : 'жд',
   'žђ' : 'жд',
+  'ǵ' : 'ꙉ',
+  'ḱ' : 'к҄',
+  'x́' : 'х',
   'ћ' : 'щ',
   'ђ' : 'жд',
-  '' : '', 
-  '' : '', 
-  '' : '', 
-  '' : '', 
-  '' : '', 
-  '' : '', 
-  '' : '', 
-  '' : '', 
-  '' : '', 
-  '' : '', 
-  '' : '', 
-  '' : '', 
-  '' : '', 
-  '' : '' 
+  'b' : 'б', 
+  'p' : 'п', 
+  'v' : 'в', 
+  'f' : 'ф', 
+  't' : 'т', 
+  'd' : 'д', 
+  's' : 'с', 
+  'z' : 'з', 
+  'ʒ' : 'ѕ', 
+  'ś' : 'с', 
+  'c' : 'ц', 
+  'k' : 'к', 
+  'g' : 'г', 
+  'x' : 'х',
+  'ü' : 'ѵ',
+  'y' : 'ъі',
+  'č' : 'ч',
+  'š' : 'ш',
+  'ž' : 'ж',
+  'Ǟ' : 'а',
+  'a' : 'а',
+  'e' : 'е',
+  'ę' : 'ѧ',
+  'ě' : 'ѣ',
+  'i' : 'и',
+  'ь' : 'ь',  
+  'ъ' : 'ъ',
+  'o' : 'о',
+  'ǫ' : 'ѫ',
+  'u' : 'оу',
+  'l' : 'л',
+  'n' : 'н',
+  'm' : 'м',
+  'r' : 'р',  
+  '' : '',
+  '' : '',
+  '' : '',
+  '' : '',
+  '' : '',
+  '' : '',
+  '' : '',
+  '' : '',  
+  '' : '',
+  '' : '',
+  '' : '',
+  '' : '',
+  '' : '',
+  '' : '',
+  '' : '',
+  '' : '',  
+  '' : '',
+  '' : '',
+  '' : '',
+  '' : '',
+  '' : '',
+  '' : '',
+  '' : '',
+  '' : '',  
+  '' : '',
+  '' : '',
+  '' : '',
+  '' : '',
+  '' : '',
+  '' : '',
+  '' : '',
 }
 
 const convertToOCS = (lcs_word) => {
@@ -112,6 +176,7 @@ const convertToOCS = (lcs_word) => {
     lcs_word = lcs_word.slice(0, PV2_pos) + PV2_map.get(PV2_cons) + lcs_word.slice(PV2_pos + 1);
     PV2_pos = lcs_word.search(PV2_regex);
   }
+  lcs_word = applyPV3(lcs_word);
   
   for(const key in mappings) {
     lcs_word = lcs_word.replaceAll(key, mappings[key]);
@@ -121,8 +186,8 @@ const convertToOCS = (lcs_word) => {
 
 // input_file.on('line', line => fs.appendFileSync("copy.csv", line+'\n'));
 input_file.on('line', line => {
-  if(line.search(ORT_regex) != -1) line += "|TORT";
-  if(line.search(PV2_regex) != -1) line += "|PV2";
+ // if(line.search(ORT_regex) != -1) line += "|TORT";
+ // if(line.search(PV2_regex) != -1) line += "|PV2";
   console.log(convertToOCS(line));
 });
 
