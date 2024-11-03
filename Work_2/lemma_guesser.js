8f3fe20040cda7329b208ffa398798a81eecb9cb
12401453 <12401453@protonmail.com>
2024-11-03 17:27:16 +0000

    ..

--- Work_2/lemma_guesser.js
+++ Work_2/lemma_guesser.js
@@ -21,8 +21,6 @@ read_stream2.on('error', () => {
   exit(-1);
 })
 
-const chu_corpus_file = readline.createInterface({input: read_stream1});
-const autotagged_pos_file = readline.createInterface({input: read_stream2});
 
 const output_filename = "chu_untagged_autolemmatised.csv";
 
@@ -30,44 +28,101 @@ let csv_string = "";
 
 const existing_normalised_map = new Map();
 const existing_raw_map = new Map();
+const lemmas_array = new Array();
 
-chu_corpus_file.on('line', line => {
-  const row = line.split(",");
-  const word_form_raw = row[0];
-  const word_form_normalised = row[3];
-  const pos = row[1];
-  const lemma_id = row[4];
 
-  const pos_form_combo_norm = pos+word_form_normalised;
-  const pos_form_combo_raw = pos+word_form_raw;
-  console.log(pos_form_combo_raw);
-  existing_normalised_map.set(pos_form_combo_norm, lemma_id);
-  existing_raw_map.set(pos_form_combo_raw, lemma_id);
 
-  console.log(`number of unique normalised POS+form combos: ${existing_normalised_map.size}\nnumber of unique raw POS+form combos:${existing_raw_map.size}`)
+async function readCorpusFile() {
+  const chu_corpus_file = readline.createInterface({input: read_stream1});
+  for await(const line of chu_corpus_file) {
+    const row = line.split(",");
+    const word_form_raw = row[0];
+    const word_form_normalised = row[3];
+    const pos = row[1];
+    const lemma_id = row[4];
+  
+    existing_normalised_map.set(pos+word_form_normalised, lemma_id);
+    existing_raw_map.set(pos+word_form_raw, lemma_id);
+  };
+  chu_corpus_file.close();
+}
 
-});
+const read_stream3 = fs.createReadStream("chu_lemmas.csv");
 
-autotagged_pos_file.on('line', line => {
-  const row = line.split(",");
-  const word_form_raw = row[2];
-  const word_form_normalised = row[0];
-  const pos = row[1];
-  let auto_lemma_id = 0;
+async function readLemmasFile() {
+  const lemmas_file = readline.createInterface({input: read_stream3});
 
-  //console.log(line);
-
-  if(existing_normalised_map.has(pos+word_form_normalised)) {
-    auto_lemma_id = existing_normalised_map.get(pos+word_form_normalised);
+  for await(const line of lemmas_file) {
+    const row = line.split(",");
+    //I don't need to remove the hashtags which disamibiguate multiple same-POS lemmas, because I will be checking whether they .startsWith() my form
+    lemmas_array.push(row);
   }
-  else if(existing_raw_map.has(pos+word_form_raw)) {
-    auto_lemma_id = existing_raw_map.get(pos+word_form_raw);
+  lemmas_file.close();
+}
+
+let unmatched_form_count = 0;
+let matched_by_lemmalist_count = 0;
+async function readAutotaggedFile() {
+  const autotagged_pos_file = readline.createInterface({input: read_stream2});
+
+  for await(const line of autotagged_pos_file) {
+    const row = line.split(",");
+    const word_form_raw = row[2];
+    const word_form_normalised = row[0];
+    const pos = row[1];
+    let auto_lemma_id = 0;
+
+    
+  
+    //console.log(line);
+  
+    if(existing_normalised_map.has(pos+word_form_normalised)) {
+      auto_lemma_id = existing_normalised_map.get(pos+word_form_normalised);
+    }
+    else if(existing_raw_map.has(pos+word_form_raw)) {
+      auto_lemma_id = existing_raw_map.get(pos+word_form_raw);
+    }
+    else {
+      unmatched_form_count++;
+      //console.log(`No matching form was found for ${word_form_raw} in the existing tagged corpus, chopping and checking against lemma-list...`)
+      let match_found = false;
+      let chopped_word_normalised = word_form_normalised;
+
+      let filtered_lemmas = lemmas_array.filter(row => row[2] == pos && row[3].startsWith(chopped_word_normalised))
+      
+      //should also check against the unnormalised forms
+      //it might be better to first check against the existing forms to catch things like пришьд- which is too dissimilar from прити to ever get correctly lemmatised this way
+      while(chopped_word_normalised.length > 3 && filtered_lemmas.length == 0) {
+
+        filtered_lemmas = lemmas_array.filter(row => row[2] == pos && row[3].startsWith(chopped_word_normalised));
+
+        if(filtered_lemmas.length > 0) {
+          auto_lemma_id = filtered_lemmas[0][0];
+          match_found = true;
+          console.log(`${word_form_raw} was matched against the lemma-list with ${filtered_lemmas[0][1]}`);
+          matched_by_lemmalist_count++;
+          break;
+        }
+        chopped_word_normalised = chopped_word_normalised.slice(0, -1);
+        
+      }
+    }
+    csv_string += word_form_raw + "," + pos + "," + auto_lemma_id + "\n";
   }
-  //else console.log("autolemmatisation by comparison with existing tagged forms was impossible");
+  autotagged_pos_file.close();
+}
 
-  csv_string += word_form_raw + "," + pos + "," + auto_lemma_id + "\n";
-});
-
-
-fs.appendFileSync(output_filename, csv_string);
+async function guessLemmas() {
+  await readCorpusFile();
+  await readLemmasFile();
+  console.log(lemmas_array.length);
+  await readAutotaggedFile();
+  
+  fs.appendFileSync(output_filename, csv_string);
+  
+  console.log(unmatched_form_count, `words could not be matched against the existing corpus-words`);
+  console.log(matched_by_lemmalist_count, `of those were matched against the lemma-list`);
+  console.log('This leaves',unmatched_form_count - matched_by_lemmalist_count, 'words with no lemma-guess at all');
+}
+guessLemmas();
 
