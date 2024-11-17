60815ba9cdf1db344b364444be2a15d272d20016
12401453 <12401453@protonmail.com>
2024-11-17 13:45:27 +0000

    ..

--- my_scripts/chu_words_sqlite.cpp
+++ my_scripts/chu_words_sqlite.cpp
@@ -3,6 +3,37 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <unordered_map>
+
+std::unordered_map <std::string, int> pos_map = {
+  {"A-", 0},
+  {"Df", 1},
+  {"S-", 2},
+  {"Ma", 3},
+  {"Nb", 4},
+  {"C-", 5},
+  {"Pd", 6},
+  {"F-", 7},
+  {"Px", 8},
+  {"N-", 9},
+  {"I-", 10},
+  {"Du", 11},
+  {"Pi", 12},
+  {"Mo", 13},
+  {"Pp", 14},
+  {"Pk", 15},
+  {"Ps", 16},
+  {"Pt", 17},
+  {"R-", 18},
+  {"Ne", 19},
+  {"Py", 20},
+  {"Pc", 21},
+  {"Dq", 22},
+  {"Pr", 23},
+  {"G-", 24},
+  {"V-", 25},
+  {"X-", 26}
+};
 
 int main () {
     sqlite3* DB;
@@ -16,7 +47,7 @@ int main () {
 
         sqlite3_exec(DB, sql_BEGIN, nullptr, nullptr, nullptr);
 
-        sqlite3_exec(DB, "DROP TABLE IF EXISTS tagged_corpus;CREATE TABLE tagged_corpus (tokno INTEGER PRIMARY KEY, chu_word_torot TEXT, chu_word_normalised TEXT, pos TEXT, morph_tag TEXT, lemma_id INTEGER, sentence_no INTEGER);CREATE INDEX lemma_id_index on tagged_corpus(lemma_id);CREATE INDEX sentence_id_index on tagged_corpus(sentence_id);", nullptr, nullptr, nullptr);
+        sqlite3_exec(DB, "DROP TABLE IF EXISTS tagged_corpus;CREATE TABLE tagged_corpus (tokno INTEGER PRIMARY KEY, chu_word_torot TEXT, chu_word_normalised TEXT, pos INTEGER, morph_tag TEXT, lemma_id INTEGER, sentence_no INTEGER);CREATE INDEX lemma_id_index on tagged_corpus(lemma_id);CREATE INDEX sentence_id_index on tagged_corpus(sentence_id);CREATE INDEX pos_index on tagged_corpus(pos)", nullptr, nullptr, nullptr);
 
         const char *sql_text = "INSERT INTO tagged_corpus (chu_word_torot, pos, morph_tag, chu_word_normalised, lemma_id, sentence_no) VALUES (?, ?, ?, ?, ?, ?)";
 
@@ -24,12 +55,26 @@ int main () {
                
         std::string line;
         std::string field;
+        int pos_key = 0;
         while(std::getline(chu_words_file, line)) {
             std::stringstream ss_line(line);
             int parameter_no = 1;
             while(std::getline(ss_line, field, ',')) {
-                if(parameter_no < 5) sqlite3_bind_text(statement, parameter_no, field.c_str(), -1, SQLITE_TRANSIENT); //SQLITE_STATIC doesnt work and makes every field get filled with some permutation of the sentence_id
-                else sqlite3_bind_int(statement, parameter_no, std::stoi(field));
+                switch(parameter_no) {
+		    case 2:
+			pos_key = pos_map.at(field);
+			sqlite3_bind_int(statement, parameter_no, pos_key);		
+			break;
+		    case 5:
+			sqlite3_bind_int(statement, parameter_no, std::stoi(field));
+			break;
+		    case 6:
+			sqlite3_bind_int(statement, parameter_no, std::stoi(field));
+			break;
+		    default:
+			sqlite3_bind_text(statement, parameter_no, field.c_str(), -1, SQLITE_TRANSIENT); //SQLITE_STATIC doesnt work and makes every field get filled with some permutation of >
+                        break;	
+		}
                 parameter_no++;
             }  
             sqlite3_step(statement);
